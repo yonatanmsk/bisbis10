18ba649c820509b13e8232d2891a1916fbef2b8b
INTL\dm865q <dm865q@intl.att.com>
2024-03-15 17:25:24 +0200

    init

---
+++ src/main/java/com/att/tdp/bis10/Bis10Application.java
@@ -0,0 +1,13 @@
+package com.att.tdp.bis10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bis10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bis10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/att/tdp/bis10/Bis10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.att.tdp.bis10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bis10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
